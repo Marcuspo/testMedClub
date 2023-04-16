23162d2ae870e9d45782b45ca1b1176cb2dc12be
Marcus Pontes <marcus.pontes@a2wplataforma.com.br>
2023-04-15 21:25:53 -0300

    change displayname app

--- ios/testMedClubTests/testMedClubTests.m
+++ ios/testMedClubTests/testMedClubTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface testMedClubTests : XCTestCase
+@interface TestMedClubTests : XCTestCase
 
 @end
 
-@implementation testMedClubTests
+@implementation TestMedClubTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
